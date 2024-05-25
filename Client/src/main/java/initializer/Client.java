3e77de3983516797c02f20a142f2e26aace1e57b
Diogo Teixeira <1200904@isep.ipp.pt>
2024-05-25 04:52:04 +0100

    #2 Added client and agent modules

---
+++ Client/src/main/java/initializer/Client.java
@@ -0,0 +1,31 @@
+package initializer;
+
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.logging.Logger;
+
+
+@SpringBootApplication
+public class Client implements CommandLineRunner {
+
+    private static Logger LOG = Logger.getLogger(Client.class.getName());
+    private final BlockingQueue<String> outputQueue = new LinkedBlockingQueue<>();
+
+    public static void main(String[] args) {
+        SpringApplication.run(Client.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws IOException, InterruptedException {
+
+
+
+
+    }
+}
--- src/main/java/READAP/ReadapApplication.java
+++ src/main/java/READAP/ReadapApplication.java
@@ -1,27 +1,36 @@
-package READAP;
+package main.java.READAP;
 
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.*;
+import java.util.Scanner;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
+class Shared{
+	volatile long startTime= 0;
+}
+
 @SpringBootApplication
-public class ReadapApplication implements CommandLineRunner{
+public class ReadapApplication implements CommandLineRunner {
 
 	private static Logger LOG = Logger.getLogger(ReadapApplication.class.getName());
+	private final BlockingQueue<String> outputQueue = new LinkedBlockingQueue<>();
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReadapApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args){
+	public void run(String... args) throws IOException, InterruptedException {
 
-		LOG.info("#Running....");
+
 
 
 	}
-
 }
